3b81e88c709eb764d2510ef7402c0f91d3e8e260
Rohan Bhajekar <rohanb1888@gmail.com>
2013-02-07 22:56:37 +0530

    Vew Flash banner added

---
+++ script/validation.js
@@ -0,0 +1,101 @@
+function validate() {
+	 var elem = document.getElementById('frmMain').elements;
+     for(var i = 0; i < elem.length; i++)
+     {
+         if (elem[i].value == "") {
+        	 if (elem[i].id == "Name" || elem[i].id == "Phone" || elem[i].id == "Email") {
+            	 continue;
+             }
+        	 alert(elem[i].name + " is mandatory..");
+        	 elem[i].focus();
+             return false;
+         }
+
+         if (elem[i].id == "Phone") {
+        	if (isNaN(elem[i].value)) {
+        		alert("Please enter Numeric Value for " + elem[i].name);
+        		elem[i].select();
+        		elem[i].focus();
+        		return false;
+        	}
+         }
+     }
+     
+     if (!validateEmail()) {
+    	 return false;
+     }
+     /*if (!validatePassword()){
+    	 return false;
+     }*/
+
+}
+// Validation for Feedback form start
+
+//function validatefeedback() {
+//	var elem = document.getElementById('frmfeedback').elements;
+//     for(var i = 0; i < elem.length; i++)
+//     { 
+//         if (elem[i].value == "") {
+//        	 alert(elem[i].name + " is mandatory..");
+//        	 elem[i].select();
+//			 elem[i].focus();
+//             return false;
+//         
+//			 if (!feedbackemail()){
+//				return false;
+//			 }
+//		} 
+//	}   
+//}
+
+//function feedbackemail() {
+//	 var f_email = document.getElementById("email");
+//				 var feed = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+//				 	if (feed.test(f_email.value) == false) 
+//					{
+//						alert('Invalid Email Address');
+//						f_email.select();
+//						f_email.focus();
+//						return false;
+//					}
+//				 return true;	 
+//}
+
+// Validation for Feedback form ends
+
+function validateEmail() {
+	var email = document.getElementById("Email");
+//	var confirmemail = document.getElementById("confirmemail");
+	
+	var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+        if (reg.test(email.value) == false) 
+        {
+            alert('Invalid Email Address');
+            email.select();
+            email.focus();
+            return false;
+        }
+
+	//if (email.value != confirmemail.value) {
+//		alert("Confirm Email incorrect");
+//		confirmemail.value = "";
+//		confirmemail.focus();
+//		return false;
+//	}
+//	return true;
+}
+
+//
+//function validatePassword() {
+//	var password = document.getElementById("password");
+//	var confirmPassword = document.getElementById("confirmPassword");
+//	
+//	if (password.value != confirmPassword.value) {
+//		alert("Confirm Password incorrect");
+//		confirmPassword.value = "";
+//		confirmPassword.focus();
+//		return false;
+//	}
+//	return true;
+//}
